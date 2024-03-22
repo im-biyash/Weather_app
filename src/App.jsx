eb932839662d0add6a537e29fcbbfc291bc4ca59
biyash <bsstha21@gmail.com>
2024-03-22 17:42:42 +0545

    weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,60 @@
 import { useState } from "react";
-import axios from "axios"; // Import axios library
+import axios from "axios";
 import Weather from "./Weather";
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
   const [location, setLocation] = useState("");
-  const API_KEY = "f9d2aaa2ba94d7f625369864581a7430";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const [searching, setSearching] = useState(false); // State to track if searching is in progress
+  const [searched, setSearched] = useState(false); // State to track if search has been performed
 
-  const searchLocation = (event) => {
+  const API_KEY = "f9d2aaa2ba94d7f625369864581a7430";
+
+  const searchLocation = () => {
+    if (location.trim() === "") return; // Don't search if location is empty
+
+    setSearching(true);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+      )
+      .then((response) => {
+        setData(response.data);
+        setSearching(false);
+        setSearched(true);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        setData(null);
+        setSearching(false);
+        setSearched(true);
+      });
+  };
+
+  const handleSearch = (event) => {
     if (event.key === "Enter") {
-      axios
-        .get(url)
-        .then((response) => {
-          setData(response.data);
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
-        });
-      setLocation("");
+      searchLocation();
     }
   };
 
   return (
     <div className="w-full h-full min-h-screen flex flex-col justify-center items-center bg-black text-white">
-      <h1 className="text-4xl font-serif mt-7 mr-3 items-center">Weather App</h1>
+      <h1 className="text-4xl font-serif relative top-[30px] mr-3 items-center">
+        Weather App
+      </h1>
       <input
         type="text"
-        className="py-3 px-4 w-full md:w-[400px] sm:w-[300px] my-12 text-lg rounded-3xl border border-gray-200 text-gray"
+        className="py-3 px-4 text-black w-full md:w-[400px] sm:w-[300px] my-12 text-lg rounded-3xl border border-gray-200 text-gray"
         value={location}
         placeholder="Enter your city"
         onChange={(event) => setLocation(event.target.value)}
-        onKeyDown={searchLocation}
+        onKeyDown={handleSearch}
       />
-      <Weather weatherdata={data} />
+      {searching && <div>Loading...</div>}
+      {searched && !searching && data === null && (
+        <span>  No data found</span>
+      )}
+      {data !== null && <Weather weatherdata={data} />}
     </div>
   );
 }
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -2,59 +2,55 @@ import React from "react";
 
 const Weather = ({ weatherdata }) => {
   return (
-    <div>
-      {weatherdata.weather ? (
-   <div className="w-[300px] md:w-[500px] h-[250px] bg-[#0a192f] text-white shadow-lg rounded-xl m-auto relative px-6 top-[10%] ">
-          <div className="flex justify-between w-full">
-            <div className="w-1/2 my-4 mx-auto flex justify-between items-center">
-              <div className="flex flex-col items-start justify-between h-full">
-                <div>
-                  <p className="text-lg font-bold ml-4">
-                    {weatherdata.name}, {weatherdata.sys.country}
-                  </p>
-                  <p className="text-sm">
-                    {weatherdata.weather[0].description}
-                  </p>
-                </div>
-                <div>
-                  <h1 className="text-6xl">
-                    {(weatherdata.main.temp - 273.15).toFixed()} °C {/* Convert temperature to Celsius */}
-                  </h1>
-                </div>
-              </div>
+    <div className="w-[300px] md:w-[500px] h-[250px] bg-[#0a192f] text-white shadow-lg rounded-xl m-auto relative px-6 top-[10%] ">
+      <div className="flex justify-between w-full">
+        <div className="w-1/2 my-4 mx-auto flex justify-between items-center">
+          <div className="flex flex-col items-start justify-between h-full">
+            <div>
+              <p className="text-lg font-bold ml-4">
+                {weatherdata.name}, {weatherdata.sys.country}
+              </p>
+              <p className="text-sm">
+                {weatherdata.weather[0].description}
+              </p>
             </div>
-            <div className="w-1/2 flex flex-col justify-between items-end">
-              <div className="relative">
-                <img
-                  src={`http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png`}
-                  alt="Weather Icon" className="color-white"
-                />
-              </div>
-              <div className="footer flex justify-between gap-x-2 ">
-                <p className=" sm:mb-3">Feels like:</p>
-                <p>{(weatherdata.main.feels_like - 273.15).toFixed()}°C</p> {/* Convert feels like temperature to Celsius */}
-              </div>
-              <div className="footer flex justify-between  gap-x-2  ">
-                <p className="sm:mb-3">Humidity:</p>
-                <p>{weatherdata.main.humidity}</p>
-              </div>
-             
+            <div>
+              <h1 className="text-6xl">
+                {(weatherdata.main.temp - 273.15).toFixed()} °C{" "}
+                {/* Convert temperature to Celsius */}
+              </h1>
             </div>
-          </div>
-          <div className="footer flex justify-around mt-14 ">
-            <p>
-              Wind Speed:
-              {weatherdata.wind.speed.toFixed()} Kph 
-            </p>
-            <p>
-              Pressure:
-              {weatherdata.main.pressure} hpa 
-            </p>
           </div>
         </div>
-      ) : (
-        <h1>No data found</h1>
-      )}
+        <div className="w-1/2 flex flex-col justify-between items-end">
+          <div className="relative">
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png`}
+              alt="Weather Icon"
+              className="color-white"
+            />
+          </div>
+          <div className="footer flex justify-between gap-x-2 ">
+            <p>Feels like:</p>
+            <p>{(weatherdata.main.feels_like - 273.15).toFixed()}°C</p>{" "}
+            {/* Convert feels like temperature to Celsius */}
+          </div>
+          <div className="footer flex justify-between  gap-x-2  ">
+            <p className="mb-3">Humidity:</p>
+            <p>{weatherdata.main.humidity}</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer flex justify-around md:mt-6 ">
+        <p>
+          Wind Speed:
+          {weatherdata.wind.speed.toFixed()} Kph
+        </p>
+        <p>
+          Pressure:
+          {weatherdata.main.pressure} hpa
+        </p>
+      </div>
     </div>
   );
 };
